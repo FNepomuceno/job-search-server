6e4d0ad0958ff27ea76337743068380f2888c917
Francis Nepomuceno <francisnepo@gmail.com>
2020-10-12 15:17:40 -0400

    Added database connection functionality

--- src/main.c
+++ src/main.c
@@ -2,5 +2,25 @@
 #include <stdio.h>
 
 int main() {
-    printf("%s\n", sqlite3_libversion());
+	sqlite3 * db; // database connection
+
+	// Open a connection to the database at `jobs.db`
+	int conn_rc = sqlite3_open("jobs.db", &db);
+
+	// If database is not opened/created successfully
+	if (conn_rc != SQLITE_OK) {
+		// Print error message
+		fprintf(stderr, "Cannot open database: %s\n",
+				sqlite3_errmsg(db));
+
+		// Close the connection
+		sqlite3_close(db);
+
+		return 1;
+	}
+
+	// Close the connection when finished
+	sqlite3_close(db);
+
+	return 0;
 }

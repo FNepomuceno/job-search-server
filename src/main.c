e112188b52ae46fb98fab4507b29d2842f9ca19e
Francis Nepomuceno <francisnepo@gmail.com>
2020-10-12 19:02:18 -0400

    Extracted connection making functionality

--- src/main.c
+++ src/main.c
@@ -1,27 +1,63 @@
 #include <sqlite3.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
+
+typedef struct db_conn
+{
+	sqlite3 * db;
+	sqlite3_stmt * res;
+	bool is_successful;
+} db_conn;
+
+void close_conn(db_conn * conn)
+{
+	sqlite3_close(conn->db);
+	conn->db = NULL;
+}
+
+db_conn * new_conn(char * filename)
+{
+	db_conn * result = malloc(sizeof(db_conn));
+
+	sqlite3 ** db_ref = &(result->db);
+	result->res = NULL;
+	result->is_successful = true;
+
+	int conn_rc = sqlite3_open(filename, db_ref);
+	if (conn_rc != SQLITE_OK)
+	{
+		result->is_successful = false;
+
+		fprintf(stderr, "Cannot open database: %s\n",
+				sqlite3_errmsg(*db_ref));
+		close_conn(result);
+	}
+
+	return result;
+}
+
+void clean_conn(db_conn ** conn_ref)
+{
+	if (*conn_ref == NULL) return;
+
+	db_conn * conn = *conn_ref;
+	close_conn(conn);
+
+	free(conn);
+	*conn_ref = NULL;
+}
 
 int main()
 {
 	// -- Connect to database --
-	sqlite3 * db; // database connection
-
-	// Open a connection to the database at `jobs.db`
-	int conn_rc = sqlite3_open("jobs.db", &db);
-
-	// If database is not opened/created successfully
-	if (conn_rc != SQLITE_OK)
+	db_conn * conn = new_conn("jobs.db");
+	sqlite3 * db = conn->db;
+	if (!(conn->is_successful))
 	{
-		// Print error message
-		fprintf(stderr, "Cannot open database: %s\n",
-				sqlite3_errmsg(db));
-
-		// Close the connection
-		sqlite3_close(db);
-
+		clean_conn(&conn);
 		return 1;
 	}
-
 
 	// -- Compile statement --
 	sqlite3_stmt * res; // prepared statement
@@ -67,8 +103,8 @@ int main()
 	// Destroy the prepared statement before closing connection
 	sqlite3_finalize(res);
 
-	// Close the connection when finished
-	sqlite3_close(db);
+	// Close the connection
+	clean_conn(&conn);
 
 	return 0;
 }

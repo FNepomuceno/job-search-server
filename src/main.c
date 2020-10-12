7e2333b77c947f1ba030955fe99a5b687f3ec540
Francis Nepomuceno <francisnepo@gmail.com>
2020-10-12 19:44:32 -0400

    Extracted statement compiling functionality

--- src/main.c
+++ src/main.c
@@ -6,9 +6,17 @@
 typedef struct db_conn
 {
 	sqlite3 * db;
-	sqlite3_stmt * res;
 	bool is_successful;
 } db_conn;
+
+typedef struct db_stmt
+{
+	sqlite3_stmt * res;
+	db_conn * conn;
+	char * query;
+	int length; // includes null terminator
+	bool is_successful;
+} db_stmt;
 
 void close_conn(db_conn * conn)
 {
@@ -16,12 +24,22 @@ void close_conn(db_conn * conn)
 	conn->db = NULL;
 }
 
+void clean_conn(db_conn ** conn_ref)
+{
+	if (*conn_ref == NULL) return;
+
+	db_conn * conn = *conn_ref;
+	close_conn(conn);
+
+	free(conn);
+	*conn_ref = NULL;
+}
+
 db_conn * new_conn(char * filename)
 {
 	db_conn * result = malloc(sizeof(db_conn));
 
 	sqlite3 ** db_ref = &(result->db);
-	result->res = NULL;
 	result->is_successful = true;
 
 	int conn_rc = sqlite3_open(filename, db_ref);
@@ -37,56 +55,70 @@ db_conn * new_conn(char * filename)
 	return result;
 }
 
-void clean_conn(db_conn ** conn_ref)
+void finalize_stmt(db_stmt * stmt)
 {
-	if (*conn_ref == NULL) return;
+	sqlite3_finalize(stmt->res);
+	stmt->res = NULL;
+}
 
-	db_conn * conn = *conn_ref;
-	close_conn(conn);
+void clean_stmt(db_stmt ** stmt_ref)
+{
+	if (*stmt_ref == NULL) return;
 
-	free(conn);
-	*conn_ref = NULL;
+	db_stmt * stmt = *stmt_ref;
+	finalize_stmt(stmt);
+
+	free(stmt);
+	*stmt_ref = NULL;
+}
+
+db_stmt * new_stmt(db_conn * conn, char * query, int length)
+{
+	db_stmt * result = malloc(sizeof(db_stmt));
+
+	sqlite3_stmt ** res_ref = &(result->res);
+	result->conn = conn;
+	result->query = query;
+	result->length = length;
+	result->is_successful = true;
+
+	if (conn == NULL || !(conn->is_successful))
+	{
+		result->res = NULL;
+		result->is_successful = false;
+		return result;
+	}
+
+	int stmt_rc = sqlite3_prepare_v2(conn->db, query, length,
+			res_ref, NULL);
+	if (stmt_rc != SQLITE_OK)
+	{
+		result->is_successful = false;
+
+		fprintf(stderr, "Failed to fetch data: %s\n",
+				sqlite3_errmsg(conn->db));
+		finalize_stmt(result);
+	}
+
+	return result;
 }
 
 int main()
 {
 	// -- Connect to database --
 	db_conn * conn = new_conn("jobs.db");
-	sqlite3 * db = conn->db;
-	if (!(conn->is_successful))
-	{
-		clean_conn(&conn);
-		return 1;
-	}
 
 	// -- Compile statement --
-	sqlite3_stmt * res; // prepared statement
-	const char stmt[] = "SELECT SQLITE_VERSION()";
-
-	// Compile statement into a byte-code program
-	int query_rc = sqlite3_prepare_v2(
-		db,
-		stmt,
-		sizeof(stmt), // only works with const char[]
-		&res,
-		NULL
-	);
-
-	// If there is an error compiling SQL statement
-	if (query_rc != SQLITE_OK)
+	char query[] = "SELECT SQLITE_VERSION();";
+	int length = sizeof(query); // only works with const char[]
+	db_stmt * stmt = new_stmt(conn, query, length);
+	sqlite3_stmt * res = stmt->res;
+	if (!(stmt->is_successful))
 	{
-		// Print error message
-		fprintf(stderr, "Failed to fetch data: %s\n",
-				sqlite3_errmsg(db));
-
-		// Statement is NULL; no need to finalize it
-
-		// Close the connection
-		sqlite3_close(db);
-
-		return 1;
+		clean_stmt(&stmt);
+		clean_conn(&conn);
+		return 10;
 	}
-
 
 	// -- Execute statement --
 	int step_rc = sqlite3_step(res);
@@ -98,12 +130,8 @@ int main()
 		printf("%s\n", sqlite3_column_text(res, 0));
 	}
 
-
 	// -- Cleanup --
-	// Destroy the prepared statement before closing connection
-	sqlite3_finalize(res);
-
-	// Close the connection
+	clean_stmt(&stmt);
 	clean_conn(&conn);
 
 	return 0;

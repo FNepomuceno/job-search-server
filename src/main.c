c2c46e6ef643f46ab8821b1e92ee79153b0cf6f3
Francis Nepomuceno <francisnepo@gmail.com>
2020-10-29 07:21:04 -0400

    Separated HTTP request parsing into a function

--- src/main.c
+++ src/main.c
@@ -15,66 +15,175 @@ void sigintHandler(int signum)
     server.is_running = false;
 }
 
-void web_send_hello(int clientfd, void * extra_data)
+typedef struct http_request
 {
-    // STEP 0: Get database connection from `extra_data`
-    db_conn * conn = (db_conn *) extra_data;
+    char * method;
+    char * uri;
+    char * version;
+    char * body;
+    char ** header_lines;
+    int num_lines;
+    int body_length;
+    bool is_successful;
+} http_request;
 
-    // STEP 1: Read and parse client request
-    // STEP 1a: Read header
-    char buffer[8192] = {0};
-    long count = read(clientfd, buffer, 8191);
-    if (count < 0)
+void clean_http_request(http_request * req)
+{
+    for (int i = 0; i < 2 * req->num_lines; i++)
     {
-        perror("In read");
-        return;
+        free(req->header_lines[i]);
     }
 
-    // STEP 1b: Find where header ends
+    free(req->method);
+    free(req->uri);
+    free(req->version);
+    free(req->body);
+    free(req->header_lines);
+}
+
+http_request read_header(int clientfd)
+{
+    http_request result;
+    char buffer[8192];
+
+    // STEP 0: Provide dummy values for struct
+    result.method = NULL;
+    result.uri = NULL;
+    result.version = NULL;
+    result.body = NULL;
+    result.header_lines = NULL;
+    result.num_lines = 0;
+    result.body_length = 0;
+    result.is_successful = false;
+
+    // STEP 1: Get request from client
+    // - Reject if read is not successful
+    long count = read(clientfd, buffer, 8191);
+    if (count < 0) return result;
+
+    // STEP 2: Find where header ends
+    // - Reject request if header does not fit within buffer
     char * header_end = strstr(buffer, "\r\n\r\n");
     if (header_end == NULL)
     {
         fprintf(stderr, "Header too large! Received:\n");
         fprintf(stderr, "%s\n", buffer);
+        return result;
     }
     long header_len = header_end - buffer;
 
-    // STEP 1c: Parse header
-    char line_end[] = "\r\n";
-
+    // STEP 3: Set up the header lines
+    // STEP 3a: Find how many header lines the request has
     char * cur_line = buffer;
     long total_length = 0;
     int line_num = 0;
 
     while (total_length < header_len)
     {
-        char * next_line = strstr(cur_line, line_end);
-        long cur_length = next_line - cur_line;
+        char * next_line = strstr(cur_line, "\r\n");
         if (next_line == NULL) break;
 
-        if (line_num == 0) // First line is the request-line
-        {
-            printf("Request-line: %.*s\n", (int)cur_length, cur_line);
-        }
-        else // Next several lines are the header-lines
-        {
-            char separator[] = ": ";
-            char * value = strstr(cur_line, separator) + 2;
-            long field_length = value - 2 - cur_line;
-            printf("Field: %.*s\n", (int)field_length, cur_line);
-            printf(
-                "Value: %.*s\n",
-                (int)(cur_length-2-field_length),
-                value
-            );
-        }
-        total_length += cur_length + 2;
+        total_length += next_line - cur_line + 2;
         cur_line = next_line + 2;
-        line_num += 1;
+        ++line_num;
     }
-    printf("Total length: %ld\n", total_length);
 
-    // STEP 1d: Obtain body if exists
+    // STEP 3b: Set up relevant data in the struct
+    // - The line count ignores the first (request-line) line
+    // - Header lines are double the number of lines to split apart the
+    //   header field and the values
+    --line_num;
+    result.num_lines = line_num;
+    result.header_lines = malloc(2 * line_num * sizeof (char *));
+    for (int i = 0; i < 2 * line_num; i++)
+    {
+        result.header_lines[i] = NULL;
+    }
+
+    // STEP 4: Parse the request line
+    char * request_line = buffer;
+
+    // STEP 4a: Get the HTTP method
+    char * method_end = strstr(request_line, " ");
+    if (method_end == NULL) return result;
+
+    long method_len = method_end - request_line;
+    result.method = malloc((method_len+1) * sizeof (char));
+    memcpy(result.method, buffer, method_len);
+    result.method[method_len] = '\0';
+
+    // STEP 4b: Get the request URI
+    char * uri_end = strstr(method_end + 1, " ");
+    if (uri_end == NULL) return result;
+
+    long uri_len = uri_end - method_end - 1;
+    result.uri = malloc((uri_len+1) * sizeof (char));
+    memcpy(result.uri, method_end+1, uri_len);
+    result.uri[uri_len] = '\0';
+
+    // STEP 4c: Get the HTTP version
+    char * version_end = strstr(uri_end + 1, "\r\n");
+    if (version_end == NULL) return result;
+
+    long version_len = version_end - uri_end - 1;
+    result.version = malloc((version_len+1) * sizeof (char));
+    memcpy(result.version, uri_end+1, version_len);
+    result.version[version_len] = '\0';
+
+    // STEP 5: Read header lines
+    cur_line = strstr(buffer, "\r\n") + 2;
+    for (int i = 0; i < result.num_lines; i++)
+    {
+        char * next_line = strstr(cur_line, "\r\n");
+        char * field_end = strstr(cur_line, ": ");
+        if (field_end == NULL) return result;
+
+        // STEP 5a: Get the field
+        long field_len = field_end - cur_line;
+        result.header_lines[2*i] = malloc((field_len+1) * sizeof (char));
+        memcpy(result.header_lines[2*i], cur_line, field_len);
+        result.header_lines[2*i][field_len] = '\0';
+
+        // STEP 5b: Get the value
+        long value_len = next_line - field_end - 2;
+        result.header_lines[2*i+1] = malloc((value_len+1) * sizeof (char));
+        memcpy(result.header_lines[2*i+1], field_end+2, value_len);
+        result.header_lines[2*i+1][value_len] = '\0';
+
+        // TODO if field is "Content-Length", save the value for STEP 6
+
+        cur_line = next_line + 2;
+    }
+
+    // STEP 6: Read body (if exists) TODO
+    // char * body_start = header_end + 4;
+
+    // STEP 7: Finalize struct
+    result.is_successful = true;
+    return result;
+}
+
+void web_send_hello(int clientfd, void * extra_data)
+{
+    // STEP 0: Get database connection from `extra_data`
+    db_conn * conn = (db_conn *) extra_data;
+
+    // STEP 1: Read and parse client request
+    http_request client_req = read_header(clientfd);
+    if (client_req.is_successful)
+    {
+        printf("METHOD: %s\n", client_req.method);
+        printf("URI: %s\n", client_req.uri);
+        printf("VERSION: %s\n", client_req.version);
+        for (int i = 0; i < client_req.num_lines; i++)
+        {
+            printf("FIELD: %s\n", client_req.header_lines[2*i]);
+            printf("VALUE: %s\n", client_req.header_lines[2*i+1]);
+        }
+    }
+
+    // Clean up struct
+    clean_http_request(&client_req);
 
     // STEP 2: Handle client request
     // STEP 2a: Obtain data from database as necessary

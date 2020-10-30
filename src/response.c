403d77f527631eeb0c757d618db608c980999cc7
Francis Nepomuceno <francisnepo@gmail.com>
2020-10-30 09:30:45 -0400

    Expanded request handling to load files

--- src/response.c
+++ src/response.c
@@ -1,26 +1,52 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "response.h"
 
-typedef struct
+http_response prepare_raw_text(char * data, int proposed_code)
 {
-    char * data;
-    int length;
-    content_type type;
-} handle_result;
+    http_response result;
 
-handle_result prepare_raw_text(char * data)
-{
-    handle_result result;
+    result.content_type = TEXT_PLAIN;
+    result.status_code = proposed_code;
 
-    result.type = TEXT_PLAIN;
     int data_len = strlen(data);
-    result.length = data_len;
+    result.content_length = data_len;
 
-    result.data = malloc((data_len+1) * sizeof (char));
-    memcpy(result.data, data, data_len);
-    result.data[data_len] = '\0';
+    result.content = malloc((data_len+1) * sizeof (char));
+    memcpy(result.content, data, data_len);
+    result.content[data_len] = '\0';
+
+    return result;
+}
+
+http_response load_from_file(char * data, int proposed_code)
+{
+    http_response result;
+    FILE * f = fopen(data, "r");
+    if (!f)
+    {
+        // Replace with loading the 404 page instead TODO
+        // Handle the 404 page not working 500 and raw text instead TODO
+        result = prepare_raw_text("Page not found", 404);
+    }
+
+    // Get the file length
+    long length;
+    fseek(f, 0, SEEK_END);
+    length = ftell(f);
+    result.content_length = (int) length;
+
+    // Get the file content
+    result.content = malloc(length + 1);
+    result.content[length] = '\0';
+    fseek(f, 0, SEEK_SET);
+    fread(result.content, 1, length, f);
+    fclose(f);
+
+    result.status_code = proposed_code;
+    result.content_type = TEXT_HTML;
 
     return result;
 }
@@ -29,33 +55,24 @@ handle_result prepare_raw_text(char * data)
 http_response handle_action(web_action * action)
 {
     http_response result;
-    result.content = NULL;
-    result.content_length = 0;
-    result.status_code = 500;
 
-    handle_result content;
     switch(action->data_type)
     {
     case ACTION_FILE_PATH:
-        // Read file TODO
-        // Replace dummy function TODO
-        content = prepare_raw_text("This is not a file, yet");
+        result = load_from_file(action->data, action->http_code);
         break;
     case ACTION_SQL_QUERY:
         // Execute SQL statement; get data if applicable TODO
+        result = prepare_raw_text("This is not a statement, yet", 500);
         break;
     case ACTION_RAW_TEXT:
-        content = prepare_raw_text(action->data);
-        result.status_code = action->http_code;
+        result = prepare_raw_text(action->data, action->http_code);
         break;
     default:
         // Behave like ACTION_RAW_TEXT with the following string literal
-        content = prepare_raw_text("You shouldn't be seeing this");
+        result = prepare_raw_text("You shouldn't be seeing this", 500);
         break;
     }
-    result.content = content.data;
-    result.content_type = content.type;
-    result.content_length = content.length;
 
     return result;
 }
